d519081454fad9f3e9604bae5ab052b57f4b0d0d
eyjhb <eyjhbb@gmail.com>
2021-01-17 13:33:34 +0100

    updated nix stuff - pinned nixpkgs

--- build.nix
+++ build.nix
@@ -1,4 +1,7 @@
-with import <nixpkgs> { };
+with import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/5d9a3e2e5a05b979d3953eeeb1a8282b241a1f53.tar.gz";
+    sha256 = "0j5q1gzciz43lsz6vh28bfp1msway91s1r4vjsnkk422a9zz1qsa";
+  }) {};
 
 let jekyll_env = bundlerEnv rec {
     name = "jekyll_env";
@@ -13,7 +16,7 @@ in stdenv.mkDerivation rec {
   src = ./.;
 
   installPhase = ''
-  export JEKYLL_ENV=production
+    export JEKYLL_ENV=production
     ${jekyll_env}/bin/jekyll build --destination $out
   '';
 }
